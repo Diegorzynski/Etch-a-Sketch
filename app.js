e1a1a9dabb95f5170a8b456f95c6bb282ff83857
diegorzynski <diegorzynski@hotmail.com>
2021-08-23 19:32:28 +0200

    fixed problem with grid sizing

--- app.js
+++ app.js
@@ -2,17 +2,30 @@ const clear = document.getElementById('clear')
 const container = document.getElementById('container')
 
 
-function makeRows(rows, columns){
-    // container.style.setProperty('--grid-rows',rows)
-    // container.style.setProperty('--grid-columns',columns)
-        for( i = 0; i < (rows * columns); i++){
-            let cell =  document.createElement('div');
-                //cell.innerText = (i + 1)
-                    container.appendChild(cell).className = 'grid-item'
-        };
+makeRows(16)
+
+
+
+function getSize(){
+    let size = parseInt(prompt('Please enter the number of columns and rows.'))
+       return makeRows(size)
 };
 
-makeRows(16,16)
+function makeRows(size){
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`
+    // container.style.cssText = `width: 500/${size}`
+    // container.style.cssText = `height: 500/${size}`
+    
+    console.log(size)
+    
+    for( i = 0; i < size * size; i++) {
+        let cell =  document.createElement('div');
+            cell.className = 'grid-item'
+                container.appendChild(cell);
+       }
+    };
+   
 
 let boxes= Array.from(document.getElementsByClassName('grid-item'))
     boxes.forEach(cel => cel.addEventListener('mouseenter', hovering));
@@ -20,8 +33,16 @@ let boxes= Array.from(document.getElementsByClassName('grid-item'))
 function hovering(e){
     e.target.style.cssText = 'background-color: purple; transition: .7s'
 }
+
 clear.addEventListener('click', clearing)
 
     function clearing(){
-        boxes.forEach(cel => cel.style.cssText = 'background-color: white')
-    }
+        boxes.forEach(cel => cel.style.cssText = 'background-color: white');
+         container.textContent = '';
+         getSize()
+        }
+        
+       
+    
+
+        
