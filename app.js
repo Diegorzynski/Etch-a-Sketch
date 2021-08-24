7757a39b965ff946d9ee73ec886a05368cecd63b
diegorzynski <diegorzynski@hotmail.com>
2021-08-24 11:08:36 +0200

    added change color btn working

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const clear = document.getElementById('clear')
 const container = document.getElementById('container')
 let originalSize = 16;
 let boxes = [];
+let color_change = document.getElementById('color-change')
+color_change.addEventListener('change',changeColor)
+let currentColor = `background-color: ${color_change.value}; transition: .7s`
+
+
 
 makeRows(originalSize);
 
@@ -30,13 +35,12 @@ function makeRows(size){
 
 
 function mousePaint() {
-
     boxes = Array.from(document.getElementsByClassName('grid-item'))
     boxes.forEach(cel => cel.addEventListener('mouseenter', hovering));
 };
 
 function hovering(e){
-    e.target.style.cssText = 'background-color: purple; transition: .7s'
+    e.target.style.cssText = currentColor;
 }
 
 clear.addEventListener('click', clearing)
@@ -45,7 +49,15 @@ clear.addEventListener('click', clearing)
         boxes.forEach(cel => cel.style.cssText = 'background-color: white');
          container.textContent = '';
          getSize()
-        }
+}
+
+
+
+function changeColor(){
+    let newColor = color_change.value;
+
+    currentColor = `background-color: ${newColor}; transition: .7s`
+}
         
        
     
